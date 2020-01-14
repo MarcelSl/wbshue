a077ec8c8a7c6f61d6eb14a503bb7dfcd8ff3c17
MarcelSl <59885881+MarcelSl@users.noreply.github.com>
2020-01-14 17:51:04 +0100

    Update server.js

--- server.js
+++ server.js
@@ -89,5 +89,5 @@ app.post('/delete',(req, res) => {
  
 //server listening
 app.listen(8001, () => {
-  console.log('Server is running at port 8000');
+  console.log('Server is running at port 8001');
 });
